a36bac448a1944398bc0424e37395964b347a811
tanhjk <tanhjk@sph.com.sg>
2024-01-22 16:32:16 +0800

    Initial push

--- build.js
+++ build.js
@@ -256,11 +256,11 @@ StyleDictionary.registerFormat({
 
 
 StyleDictionary.registerFormat({
-  name: 'dtSCSSFormat',
+  name: 'dtSCSSFormatLumos',
   formatter: function ({ dictionary }) {
     let strTokens = `//design tokens \n`;
 
-    const obj = dictionary.tokens.designtokens; 
+    const obj = dictionary.tokens.designtokens.lumos; 
 
     // strTokens += `const theme = {\n\t `;
     for (let x in obj) {
@@ -335,6 +335,89 @@ StyleDictionary.registerFormat({
     return strTokens;
   },
 });
+
+
+StyleDictionary.registerFormat({
+  name: 'dtSCSSFormatCue',
+  formatter: function ({ dictionary }) {
+    let strTokens = `//design tokens \n`;
+
+    const obj = dictionary.tokens.designtokens.cue; 
+
+    // strTokens += `const theme = {\n\t `;
+    for (let x in obj) {
+
+      strTokens += `\n//${kebabToCC(x)} tokens\n`
+      let designTokens = obj[x];
+      
+      for (let it in designTokens) {
+
+        if(designTokens[it].type === 'dimension'){
+          strTokens += `$${kebabToCC(it)}: ${designTokens[it].value}px; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          }else{
+            // console.log(x);
+            switch(x){
+              case 'texts' :
+              case 'links' :
+                strTokens += `@mixin ${kebabToCC(it)}{ \n color: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n}\n`
+                break; 
+              case 'backgrounds' :
+                strTokens += `@mixin ${kebabToCC(it)}{ \n background-color: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n}\n`
+                break;
+              case 'icons' :
+                  strTokens += `@mixin ${kebabToCC(it)}{ \n color: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n}\n`
+                  break; 
+              case 'charts':
+                let chartTokens = designTokens[it];
+                // console.log(chartTokens, it);
+                for (let ct in chartTokens){
+                  // console.log(ct, '-',it);
+                  if(chartTokens[ct].type === 'dimension'){
+                  strTokens += `$${kebabToCC(it+'-'+ct)}: ${chartTokens[ct].value}px; ${chartTokens[ct].description ? '/**' + chartTokens[ct].description +  '**/' : ''}  \n`
+                  }else{
+                  strTokens += `$${kebabToCC(it+'-'+ct)}: $${chartTokens[ct].value}; ${chartTokens[ct].description ? '/**' + chartTokens[ct].description +  '**/' : ''}\n`
+                  }
+                }
+                break; 
+              case 'buttons':
+              case 'checkbox':
+              case 'toggle':
+              case 'borders':
+                if(it.indexOf('border') >= 0){
+                  strTokens += `@mixin ${kebabToCC(it)}{ \n border-color: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n}\n`
+                }else{
+                  strTokens += `@mixin ${kebabToCC(it)}{ \n background-color: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n}\n`
+                }
+                break; 
+              default: 
+                strTokens += `$${kebabToCC(it)}: $${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+                break;
+
+            } 
+       
+          }
+           
+      }
+      // strTokens += `\t\t},\n`;
+    }
+    // strTokens += `}`;
+    dictionary.allTokens.map((tkn) => {
+      if (dictionary.usesReference(tkn.original.value)) {
+        // Note: make sure to use `token.original.value` because
+        // `token.value` is already resolved at this point.
+        const refs = dictionary.getReferences(tkn.original.value);
+        refs.forEach((ref) => { 
+          // styleVal = ref;
+          strTokens = strTokens.replace(ref.value, function () {
+            return `${ref.path[2]}`;
+          });
+        });
+      }
+    });
+    return strTokens;
+  },
+});
+
 StyleDictionary.registerFormat({
   name: 'tsJSFormat',
   formatter: function ({ dictionary }) {
@@ -435,11 +518,81 @@ let fs ;
 }
 
 StyleDictionary.registerFormat({
-  name: 'tsSCSSFormat',
+  name: 'tsSCSSFormatLumos',
   formatter: function ({ dictionary }) {
     let strTokens = `//design tokens \n`;
 
-    const obj = dictionary.tokens.typography; 
+    const obj = dictionary.tokens.typography.lumos; 
+
+    // strTokens += `const theme = {\n\t `;
+    for (let x in obj) {
+ 
+      strTokens += `\n@mixin ${kebabToCC(x)} {\n`
+      let designTokens = obj[x];
+      
+      for (let it in designTokens) {  
+
+        switch(it){
+          case 'fontSize':
+            strTokens += `\t${kebabize(it)}: ${findIdentitiesByVal(designTokens[it].value,  it)}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+          case 'textDecoration':
+            strTokens += `\t${kebabize(it)}: ${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+          case 'fontWeight':
+            strTokens += `\t${kebabize(it)}: ${findIdentitiesByVal(designTokens[it].value,  it)};  ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+          case 'fontStyle':
+            strTokens += `\t${kebabize(it)}: ${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+          case 'letterSpacing':
+            strTokens += `\t${kebabize(it)}: ${findIdentitiesByVal(designTokens[it].value,  it)}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+          case 'lineHeight':
+            if(x.indexOf('title') >=0){
+            strTokens += `\t${kebabize(it)}: $lhHeadline; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+            }else{
+              strTokens += `\t${kebabize(it)}: $lhParagraph; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+            }
+          break;
+          case 'textCase':
+            strTokens += `\ttext-transform: ${designTokens[it].value}; ${designTokens[it].description ? '/**' + designTokens[it].description+ ' **/' : ''}\n`
+          break;
+
+          default: 
+          break;
+        }
+
+      }
+      strTokens += `}\n`;
+    } 
+    // strTokens += `}`;
+     
+
+    // dictionary.allTokens.map((tkn) => { 
+    //   if (dictionary.usesReference(tkn.original.value)) {
+    //     // Note: make sure to use `token.original.value` because
+    //     // `token.value` is already resolved at this point.
+    //     const refs = dictionary.getReferences(tkn.original.value);
+    //     refs.forEach((ref) => { 
+         
+    //       strTokens = strTokens.replace(ref.value, function () {
+    //         return `${ref.path[2]}`;
+    //       });
+    //     });
+    //   }
+    // });
+    return strTokens;
+  },
+});
+
+
+StyleDictionary.registerFormat({
+  name: 'tsSCSSFormatCue',
+  formatter: function ({ dictionary }) {
+    let strTokens = `//design tokens \n`;
+
+    const obj = dictionary.tokens.typography.cue; 
 
     // strTokens += `const theme = {\n\t `;
     for (let x in obj) {
@@ -528,14 +681,14 @@ const myStyleDictionary = StyleDictionary.extend({
         },
       ],
     },
-    'scss/base': {
+    'scss/baseLumos': {
       transforms: [
         'attribute/cti', 
         'name/cti/kebab',
         'size/px',
         'color/hex',
       ],
-      buildPath: `${dsm_dir}scss/`,
+      buildPath: `${dsm_dir}scss/lumos/`,
       files: [
         {
           destination: `_base.scss`,  
@@ -547,76 +700,133 @@ const myStyleDictionary = StyleDictionary.extend({
         },
       ],
     },
-    'js/designTokens': {
+    'scss/baseCue': {
       transforms: [
-        'attribute/cti',
-        'name/cti/kebab', 
+        'attribute/cti', 
+        'name/cti/kebab',
         'size/px',
         'color/hex',
       ],
-      buildPath: `${dsm_dir}js/`,
+      buildPath: `${dsm_dir}scss/cue/`,
       files: [
         {
-          destination: `_designTokens.js`,  
-          filter: 'dsFilter',
-          format: 'dtFormate',
+          destination: `_base.scss`,  
+          filter: 'baseFilter',
+          format: 'baseSCSSFormat',
           options: {
             outputReferences: true, 
           }, 
         },
       ],
     },
-    'scss/designTokens': {
+    // 'js/designTokens': {
+    //   transforms: [
+    //     'attribute/cti',
+    //     'name/cti/kebab', 
+    //     'size/px',
+    //     'color/hex',
+    //   ],
+    //   buildPath: `${dsm_dir}js/`,
+    //   files: [
+    //     {
+    //       destination: `_designTokens.js`,  
+    //       filter: 'dsFilter',
+    //       format: 'dtFormate',
+    //       options: {
+    //         outputReferences: true, 
+    //       }, 
+    //     },
+    //   ],
+    // },
+    'scss/designTokensLumos': {
       transforms: [
         'attribute/cti',
         'name/cti/kebab', 
         'size/px',
         'color/hex',
       ],
-      buildPath: `${dsm_dir}scss/`,
+      buildPath: `${dsm_dir}scss/lumos/`,
       files: [
         {
           destination: `_designTokens.scss`,  
           filter: 'dsFilter',
-          format: 'dtSCSSFormat',
+          format: 'dtSCSSFormatLumos',
           options: {
             outputReferences: true, 
           }, 
         },
       ],
     },
-    'js/textStyles': {
+    'scss/designTokensCue': {
       transforms: [
         'attribute/cti',
         'name/cti/kebab', 
-        // 'size/px',
+        'size/px',
         'color/hex',
       ],
-      buildPath: `${dsm_dir}js/`,
+      buildPath: `${dsm_dir}scss/cue/`,
       files: [
         {
-          destination: `_typography.js`,  
-          filter: 'tsFilter',
-          format: 'tsJSFormat',
+          destination: `_designTokens.scss`,  
+          filter: 'dsFilter',
+          format: 'dtSCSSFormatCue',
           options: {
             outputReferences: true, 
           }, 
         },
       ],
     },
-    'scss/textStyles': {
+    // 'js/textStyles': {
+    //   transforms: [
+    //     'attribute/cti',
+    //     'name/cti/kebab', 
+    //     // 'size/px',
+    //     'color/hex',
+    //   ],
+    //   buildPath: `${dsm_dir}js/`,
+    //   files: [
+    //     {
+    //       destination: `_typography.js`,  
+    //       filter: 'tsFilter',
+    //       format: 'tsJSFormat',
+    //       options: {
+    //         outputReferences: true, 
+    //       }, 
+    //     },
+    //   ],
+    // },
+    'scss/textStylesLumos': {
       transforms: [
         'attribute/cti',
         'name/cti/kebab', 
         // 'size/px',
         'color/hex',
       ],
-      buildPath: `${dsm_dir}scss/`,
+      buildPath: `${dsm_dir}scss/lumos/`,
       files: [
         {
           destination: `_typography.scss`,  
           filter: 'tsFilter',
-          format: 'tsSCSSFormat',
+          format: 'tsSCSSFormatLumos',
+          options: {
+            outputReferences: true, 
+          }, 
+        },
+      ],
+    },
+    'scss/textStylesCue': {
+      transforms: [
+        'attribute/cti',
+        'name/cti/kebab', 
+        // 'size/px',
+        'color/hex',
+      ],
+      buildPath: `${dsm_dir}scss/cue/`,
+      files: [
+        {
+          destination: `_typography.scss`,  
+          filter: 'tsFilter',
+          format: 'tsSCSSFormatCue',
           options: {
             outputReferences: true, 
           }, 
